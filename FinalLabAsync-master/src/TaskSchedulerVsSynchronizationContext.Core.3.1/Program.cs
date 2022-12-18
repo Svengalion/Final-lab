b4902455c76d6f066cd174d0acecec0be9d50284
Svengalion <yung_we@mail.ru>
2022-12-18 15:50:15 -0800

    дописал комменты
    я просто хотел спать

--- FinalLabAsync-master/src/TaskSchedulerVsSynchronizationContext.Core.3.1/Program.cs
+++ FinalLabAsync-master/src/TaskSchedulerVsSynchronizationContext.Core.3.1/Program.cs
@@ -19,15 +19,15 @@ namespace TaskSchedulerVsSynchronizationContext
         private static async Task WorkAsync(object? _)//та же функция, но асинхронная
         {
             Task task = Task.Delay(1000);
-            await task;//отправляет task в работу
+            await task;//параллельно отправляет task в работу
             //await task.ConfigureAwait(false); 
             WriteLine($"Method WorkAsync (task №{task.Id}) is completed in {Thread.CurrentThread.ManagedThreadId}");
         }
 
         #region Singleton
-        private static MinThreadTaskScheduler? _scheduler;
+        private static MinThreadTaskScheduler? _scheduler; //тут в общем ленивая инициализация, нам в падлу создавать объект сейчас поэтому создастся он при первом использовании
         private static MinThreadTaskScheduler GetScheduler => 
-            _scheduler ??= new MinThreadTaskScheduler();
+            _scheduler ??= new MinThreadTaskScheduler(); //короче вот эта тема(??=) присвает _scheduler new MinThreadTaskScheduler если _scheduler = null
 
         #endregion
 
@@ -36,22 +36,22 @@ namespace TaskSchedulerVsSynchronizationContext
             WriteLine($"Method Main is started in {Thread.CurrentThread.ManagedThreadId}");
 
             Task task = new Task(Work!, null); //создает новый поток выполняющий Work
-            //task.Start();
-            //task.Wait(); //в основном потоке будет ожидать
-            //task.Start(GetScheduler); //запускает с помощью планировщика и выбирает дефолтный (если не изменен, то это основной поток программы) параллельности не будет, всче находится в основном потоке
+            //task.Start(); //тут челик говорит стартуем
+            //task.Wait(); //режим ждуна
+            //task.Start(GetScheduler); //запускает с помощью планировщика и выбирает дефолтный (если не изменен, то это основной поток программы) параллельности не будет, все находится в основном потоке
             //task.Start(TaskScheduler.FromCurrentSynchronizationContext()); // запустит поток в нынешнем контексте синхронизации
             //task.Start(TaskScheduler.Current); // запустит поток в выполняющемся потоке
-            task = Task.Factory.StartNew(() =>
+            task = Task.Factory.StartNew(() => //кент создает и запускает новую factory
             {
                 WriteLine($"Current TaskScheduler - {TaskScheduler.Current.GetType().FullName}"); //возвращает название типа текущего планировщика задач
                 Task.Run(() => WriteLine($"\tThe nested-task  (task №{Task.CurrentId}) is completed in {Thread.CurrentThread.ManagedThreadId}"));//запускает новый task 
                 Task.Factory.StartNew(() => WriteLine($"\tThe child-task (task №{Task.CurrentId}) is completed in {Thread.CurrentThread.ManagedThreadId}"),
                     TaskCreationOptions.AttachedToParent/*передает параметры создания из внешнего factory в этот*/); //создает и запускает factory 
                 WriteLine($"Task \"task\" (task №{Task.CurrentId}) is completed in {Thread.CurrentThread.ManagedThreadId}");
-            }, CancellationToken.None, TaskCreationOptions.None, GetScheduler); // HideScheduler
-            task.Wait();
-            await WorkAsync(null);
-            await WorkAsync(null).ConfigureAwait(false);
+            }, CancellationToken.None, TaskCreationOptions.None, GetScheduler/*это короче параметры создания фактори*/); // HideScheduler
+            task.Wait();//тут поток врубает режим ждуна
+            await WorkAsync(null); //параллельно выполняет WorkAsync
+            await WorkAsync(null).ConfigureAwait(false); //не особо понял что это, но эта хрень улучшает производительность и исключает дэдлоки короче ПАМАГИТИ
 
             ReadKey();
         }
